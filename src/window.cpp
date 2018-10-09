d86966144f31afbf67cadaaee37291c63aaa44f5
Niklas K <drdaxxy@googlemail.com>
2018-10-09 14:00:03 +0200

    explicitly request 8bpc framebuffer

--- src/window.cpp
+++ src/window.cpp
@@ -92,6 +92,10 @@ void TryCreateGL(GraphicsApi api) {
     g_SDLWindow = NULL;
   }
 
+  SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
+  SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
+  SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
+
   SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 0);
   SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
 
