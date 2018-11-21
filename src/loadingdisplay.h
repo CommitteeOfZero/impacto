fb25f6070f51694ec76cd09d9b742f308576396f
Andrew M <andrew@projectfilmnext.com>
2018-11-21 18:23:41 +0200

    Get rid of an extern in LoadingDisplay

--- src/loadingdisplay.h
+++ src/loadingdisplay.h
@@ -5,8 +5,6 @@ namespace LoadingDisplay {
 
 enum LoadingAnimState { Hidden, Hiding, Showing, Shown };
 
-extern LoadingAnimState AnimState;
-
 void Update(float dt);
 void Render();
 }  // namespace LoadingDisplay
