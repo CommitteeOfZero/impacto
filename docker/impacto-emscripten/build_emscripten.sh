e33f17a7dc097ac4f05dcb4b1116bad3f2a0b9d2
Niklas K <drdaxxy@googlemail.com>
2019-01-04 17:32:23 +0100

    more beautiful emscripten workarounds

--- docker/impacto-emscripten/build_emscripten.sh
+++ docker/impacto-emscripten/build_emscripten.sh
@@ -2,3 +2,5 @@
 
 emcmake cmake -DCMAKE_FIND_ROOT_PATH=/impacto-deps -DCMAKE_PREFIX_PATH=/local $@ /src
 emmake make
+# https://github.com/kripken/emscripten/issues/5436
+sed -i 's/$legalf32//g' impacto.js
