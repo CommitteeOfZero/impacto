1668ab3b63a1ec5d8e99859b2522b9ea176c7fe1
Enorovan <68027106+Enorovan@users.noreply.github.com>
2020-09-03 02:15:16 +0200

    Comments for clarity

--- src/ui/widgets/chlcc/titlebutton.cpp
+++ src/ui/widgets/chlcc/titlebutton.cpp
@@ -33,53 +33,53 @@ void TitleButton::Render() {
       Renderer2D::DrawSprite(FocusedSprite, glm::vec2(Bounds.X, Bounds.Y),
                              white);
       switch (TitleButton::Id) {
-        case 4: {
+        case 4: {  //Load to Load line
           Renderer2D::DrawSprite(
               ItemUpLine,
               glm::vec2(SecondaryMenuLineX, SecondaryMenuLoadLineY), white);
         } break;
-        case 5: {
+        case 5: {  //Load to Quick Load line
           Renderer2D::DrawSprite(
               ItemStraightLine,
               glm::vec2(SecondaryMenuLineX, SecondaryMenuLoadQuickLineY),
               white);
         } break;
-        case 6: {
+        case 6: {  // Extra to Clear List line
           Renderer2D::DrawSprite(
               ItemHyperUpLine,
               glm::vec2(SecondaryMenuLineX, SecondaryMenuExtraClearY), white);
         } break;
-        case 7: {
+        case 7: {  // Extra to CG Library line
           Renderer2D::DrawSprite(
               ItemSuperUpLine,
               glm::vec2(SecondaryMenuLineX, SecondaryMenuExtraCGY), white);
         } break;
-        case 8: {
+        case 8: {  // Extra to Sound Library line
           Renderer2D::DrawSprite(
               ItemUpLine,
               glm::vec2(SecondaryMenuLineX, SecondaryMenuExtraSoundY), white);
         } break;
-        case 9: {
+        case 9: {  // Extra to Movie Library line
           Renderer2D::DrawSprite(
               ItemStraightLine,
               glm::vec2(SecondaryMenuLineX, SecondaryMenuExtraMovieY), white);          
         } break;
-        case 10: {
+        case 10: {  // Extra to Tips line
           Renderer2D::DrawSprite(
               ItemDownLine,
               glm::vec2(SecondaryMenuLineX, SecondaryMenuExtraTipsY), white);
         } break;
-        case 11: {
+        case 11: {  // Extra to Trophy line
           Renderer2D::DrawSprite(
               ItemSuperDownLine,
               glm::vec2(SecondaryMenuLineX, SecondaryMenuExtraTrophyY), white);
         } break;
-        case 12: {
+        case 12: {  // System to Config line 
           Renderer2D::DrawSprite(
               ItemUpLine,
               glm::vec2(SecondaryMenuLineX, SecondaryMenuSystemConfigY), white);
         } break;
-        case 13: {
+        case 13: {  // System to System Save line 
           Renderer2D::DrawSprite(
               ItemStraightLine,
               glm::vec2(SecondaryMenuLineX, SecondaryMenuSystemSaveY), white);
