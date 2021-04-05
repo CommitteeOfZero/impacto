a0990f35e64ebd3464e125dc2af6ce941acac56d
Enorovan <enorovan@gmail.com>
2021-04-05 16:41:21 +0200

    fetch

---
+++ src/profile/games/chlcc/systemmenu.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "../../../spritesheet.h"
+
+namespace Impacto {
+namespace Profile {
+namespace CHLCC {
+namespace SystemMenu {
+
+void Configure();
+
+}  // namespace SystemMenu
+}  // namespace CHLCC
+}  // namespace Profile
+}  // namespace Impacto
