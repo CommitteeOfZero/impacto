62abaf6effc7a15f9ab84fc387e1ff4be4b3beb9
Enorovan <68027106+Enorovan@users.noreply.github.com>
2021-11-14 14:43:35 +0100

    add save entry button placeholders from mo6tw

---
+++ src/ui/widgets/chlcc/saveentrybutton.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include "../button.h"
+
+namespace Impacto {
+namespace UI {
+namespace Widgets {
+namespace CHLCC {
+
+class SaveEntryButton : public Widgets::Button {
+ public:
+  SaveEntryButton(int id, Sprite const& norm, Sprite const& focused,
+                  Sprite const& highlight, glm::vec2 pos)
+      : Widgets::Button(id, norm, focused, highlight, pos) {}
+  void Render() override;
+  void AddPlayTimeHintText(uint8_t* str, int fontSize, bool outline);
+  void AddPlayTimeText(uint8_t* str, int fontSize, bool outline);
+  void AddSaveDateHintText(uint8_t* str, int fontSize, bool outline);
+  void AddSaveDateText(uint8_t* str, int fontSize, bool outline);
+  void AddSceneTitleText(uint8_t* str, int fontSize, bool outline);
+
+  Sprite Thumbnail;
+
+  bool EntryActive = false;
+
+ private:
+  ProcessedTextGlyph SceneTitle[255];
+  int SceneTitleLength;
+  ProcessedTextGlyph PlayTimeHint[64];
+  int PlayTimeHintLength;
+  ProcessedTextGlyph PlayTime[255];
+  int PlayTimeLength;
+  ProcessedTextGlyph SaveDateHint[64];
+  int SaveDateHintLength;
+  ProcessedTextGlyph SaveDate[255];
+  int SaveDateLength;
+};
+
+}  // namespace MO6TW
+}  // namespace Widgets
+}  // namespace UI
+}  // namespace Impacto
