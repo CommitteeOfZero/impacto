55d1bbc06a470fd4ec8540479c186a10eead3cd6
Andrew M <andrew@projectfilmnext.com>
2024-04-12 21:18:06 +0300

    SC3 script debugger real??

--- src/game.h
+++ src/game.h
@@ -26,11 +26,12 @@ namespace Game {
 
 BETTER_ENUM(DrawComponentType, uint8_t, Text = 0x0, Main = 0x1,
             ExtrasScenes = 0x2, Mask = 0x3, SystemText = 0x3, SaveMenu = 0x4,
-            SystemIcons = 0x6, TitleMenu = 0x7, Option = 0x9, SystemMenu = 0xA,
-            SystemMessage = 0xB, PlayData = 0xC, Album = 0xD,
-            ExtrasMusicMode = 0xE, ExtrasMovieMode = 0x10,
+            SaveMenu05 = 0x5, SystemIcons = 0x6, TitleMenu = 0x7, Option = 0x9,
+            SystemMenu = 0xA, SystemMessage = 0xB, PlayData = 0xC, Album = 0xD,
+            ExtrasMusicMode = 0xE, DictionaryMode = 0xF, ExtrasMovieMode = 0x10,
             ExtrasActorsVoice = 0x11, SaveIcon = 0x12,
-            GlobalSystemMessage = 0x15, DebugEditor = 0x1E, None = 0xFF)
+            GlobalSystemMessage = 0x15, InstallInfo = 0x16,
+            SystemMessage17 = 0x17, DebugEditor = 0x1E, None = 0xFF)
 
 void InitFromProfile(std::string const& name);
 
