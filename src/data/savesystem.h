aab95af6cf8b319be34070519dc8d6717159152c
Dextinfire <>
2024-05-30 01:53:15 -0700

    Add CCLCC to savedatatype

--- src/data/savesystem.h
+++ src/data/savesystem.h
@@ -9,7 +9,7 @@
 namespace Impacto {
 namespace SaveSystem {
 
-BETTER_ENUM(SaveDataType, int, None, CHLCC, MO6TW)
+BETTER_ENUM(SaveDataType, int, None, CHLCC, CCLCC, MO6TW)
 
 enum SaveError {
   SaveOK = 0,
