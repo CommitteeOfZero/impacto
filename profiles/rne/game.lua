0c799ffefd10e73b83376a574bb7625a5f6caddb
Enorovan <enorovan@gmail.com>
2024-06-05 22:43:31 +0200

    feature/lua-config-for-realboot: expose resolution and fullscreen to config

--- profiles/rne/game.lua
+++ profiles/rne/game.lua
@@ -19,6 +19,7 @@ root.Vm = {
 
 include('common/scriptinput.lua')
 include('common/scriptvars.lua');
+include('rne/config.lua');
 include('rne/savedata.lua');
 include('rne/tipssystem.lua');
 include('rne/vfs.lua');
