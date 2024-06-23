45c272dd500266632f582893a6a3422e3f81dbc8
Dextinfire <>
2024-06-22 20:03:08 -0700

    Mistype while rebasing to lua

--- profiles/chlcc/achievementsystem.lua
+++ profiles/chlcc/achievementsystem.lua
@@ -1,4 +1,4 @@
 root.AchievementData = {
   Type = AchievementDataType.CHLCC,
-  AchievementDataPath + "games/chlcc/trophydata/TROPHY.TRP"
+  AchievementDataPath = "games/chlcc/trophydata/TROPHY.TRP"
 }
