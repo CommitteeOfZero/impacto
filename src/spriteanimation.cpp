1cba0ec65f806d7c00a35876dcbf2817560bd7ad
PringlesGang <liamelderbroek123@gmail.com>
2024-07-14 16:19:17 +0200

    Fixed SpriteAnimation to properly hold on last frame

--- src/spriteanimation.cpp
+++ src/spriteanimation.cpp
@@ -5,6 +5,8 @@ namespace Impacto {
 bool SpriteAnimation::Exists() { return Def != 0; }
 Sprite SpriteAnimation::CurrentSprite() {
   int frame = (int)(Progress * (float)Def->FrameCount);
+  if (frame >= Def->FrameCount) frame = Def->FrameCount - 1;
+  if (frame < 0) frame = 0;
   return Def->Frames[frame];
 }
 SpriteAnimation SpriteAnimationDef::Instantiate() {
