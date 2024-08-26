1a9dadc1cb392e7aa71921853c1b6c67ee7ab2ad
Dextinfire <>
2024-08-25 20:41:14 -0700

    CCLCC Tips system

---
+++ src/games/cclcc/tipssystem.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "../../data/tipssystem.h"
+
+namespace Impacto {
+namespace CCLCC {
+
+using namespace Impacto::TipsSystem;
+
+class TipsSystem : public TipsSystemBase {
+ public:
+  TipsSystem(int maxTipsCount) : TipsSystemBase(maxTipsCount) {};
+
+  void DataInit(int scriptBufferId, uint8_t* tipsData);
+  void UpdateTipRecords();
+  void SetTipLockedState(int id, bool state);
+  void SetTipUnreadState(int id, bool state);
+  void SetTipNewState(int id, bool state);
+
+  bool GetTipLockedState(int id);
+};
+
+}  // namespace CCLCC
+}  // namespace Impacto
