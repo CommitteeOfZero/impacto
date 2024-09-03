f87c87f1be8db4b315942d18ba232f8d49047adc
Dextinfire <>
2024-09-02 23:12:07 -0700

    Shader update

--- src/shaders/dx9/MaskedSpriteNoAlpha_frag.hlsl
+++ src/shaders/dx9/MaskedSpriteNoAlpha_frag.hlsl
@@ -1,6 +1,5 @@
 float2 Alpha : register(c0);
 bool IsInverted : register(b0);
-bool IsSameTexture : register(b1);
 
 struct PS_INPUT {
   float4 position : POSITION;
@@ -12,21 +11,22 @@ struct PS_INPUT {
 sampler ColorMap : register(s0);
 sampler Mask : register(s1);
 
+float rgbToLightness(float3 rgb) {
+  float maxVal = max(max(rgb.r, rgb.g), rgb.b);
+  float minVal = min(min(rgb.r, rgb.g), rgb.b);
+
+  return (maxVal + minVal) / 2.0;
+}
+
 float4 main(PS_INPUT input) : COLOR {
   float4 color = tex2D(ColorMap, input.uv);
   float4 alp = tex2D(Mask, input.maskUV);
-  float maskAlpha = (alp.r + alp.g + alp.b) / 3.0f;
+  float maskAlpha = rgbToLightness(alp.rgb);
   if (IsInverted) maskAlpha = 1.0f - maskAlpha;
   maskAlpha *= Alpha.x;
   maskAlpha -= Alpha.y;
   maskAlpha = clamp(maskAlpha, 0.0, 1.0);
-  if (IsSameTexture) {
-    color.a = maskAlpha;
-    color.a *= input.tint.a;
-  } else {
-    color *= input.tint;
-    color.a *= maskAlpha;
-  }
-
+  color *= input.tint;
+  color.a *= maskAlpha;
   return color;
 }
