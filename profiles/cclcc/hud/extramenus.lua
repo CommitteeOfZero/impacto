589e3fca61e13614c7ae397e7d9291d8957226a6
Enorovan <enorovan@gmail.com>
2024-09-23 21:55:16 +0200

    cclcc-librarymenu: add most common Library and Album textures

--- profiles/cclcc/hud/extramenus.lua
+++ profiles/cclcc/hud/extramenus.lua
@@ -40,3 +40,56 @@ root.Sprites["ClearListMask"] = {
     Sheet = "MenuChip",
     Bounds = { X = 154, Y = 140, Width = 1900, Height = 1061 },
 };
+
+-- LibraryMenu
+
+root.Sprites["LibraryBackground"] = {
+    Sheet = "LibraryMenu",
+    Bounds = { X = 0, Y = 700, Width = 1629, Height = 1080 },
+};
+
+root.Sprites["LibraryIndex"] = {
+    Sheet = "LibraryMenu",
+    Bounds = { X = 1658, Y = 0, Width = 381, Height = 1049 },
+};
+
+root.Sprites["LibraryButtonGuide"] = {
+    Sheet = "LibraryMenu",
+    Bounds = { X = 0, Y = 1780, Width = 1926, Height = 57 },
+};
+
+root.Sprites["AlbumButtonGuide"] = {
+    Sheet = "LibraryMenu",
+    Bounds = { X = 0, Y = 1878, Width = 1926, Height = 57 },
+};
+
+root.Sprites["SnapPhotoHover"] = {
+    Sheet = "LibraryMenu",
+    Bounds = { X = 0, Y = 0, Width = 252, Height = 300 },
+};
+
+root.Sprites["HitSongsHover"] = {
+    Sheet = "LibraryMenu",
+    Bounds = { X = 256, Y = 0, Width = 252, Height = 300 },
+};
+
+root.Sprites["LoveMovieHover"] = {
+    Sheet = "LibraryMenu",
+    Bounds = { X = 512, Y = 0, Width = 252, Height = 300 },
+};
+
+root.Sprites["SnapPhotoSelecting"] = {
+    Sheet = "LibraryMenu",
+    Bounds = { X = 0, Y = 365, Width = 350, Height = 300 },
+};
+
+root.Sprites["HitSongsHover"] = {
+    Sheet = "LibraryMenu",
+    Bounds = { X = 352, Y = 365, Width = 350, Height = 300 },
+};
+
+root.Sprites["LoveMovieHover"] = {
+    Sheet = "LibraryMenu",
+    Bounds = { X = 704, Y = 365, Width = 350, Height = 300 },
+};
+
