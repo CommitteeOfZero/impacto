684f91cbcca07bf71bbbea8a312f38924b311fa3
Dextinfire <>
2024-10-05 00:14:17 -0700

    Physical file stream api with more specific flags, create parent directories, better error printing

--- src/io/physicalfilestream.h
+++ src/io/physicalfilestream.h
@@ -3,6 +3,7 @@
 #include "stream.h"
 #include <fstream>
 #include <filesystem>
+#include <system_error>
 #include "buffering.h"
 
 namespace Impacto {
@@ -10,33 +11,45 @@ namespace Io {
 
 class PhysicalFileStream : public Stream {
  public:
+  enum CreateFlagsMode {
+    READ = 1,
+    WRITE = READ << 1,
+    CREATE_IF_NOT_EXISTS = WRITE << 1,
+    TRUNCATE = CREATE_IF_NOT_EXISTS << 1,
+    APPEND = TRUNCATE << 1,
+    CREATE_DIRS = CREATE_IF_NOT_EXISTS << 1
+  };
+  using CreateFlags = int;
   static IoError Create(std::string const& fileName, Stream** out,
-                        bool exists = true);
+                        CreateFlags flags = CreateFlagsMode::READ);
   int64_t Read(void* buffer, int64_t sz) override;
   int64_t Seek(int64_t offset, int origin) override;
   IoError Duplicate(Stream** outStream) override;
   int64_t Write(void* buffer, int64_t sz, int cnt = 1) override;
 
  protected:
-  std::ios_base::openmode GetFileMode(bool truncate) {
-    // trunc will clear file if it exists, and allows creation of new file
-    return (truncate) ? std::ios::in | std::ios::out | std::ios::trunc |
-                            std::ios::binary
-                      : std::ios::in | std::ios::out | std::ios::binary;
-  }
-  PhysicalFileStream(std::filesystem::path filePath, bool truncate = false)
-      : Truncate(truncate),
+  std::ios_base::openmode PrepareFileOpenMode(CreateFlags flags,
+                                              std::error_code& ec);
+
+  PhysicalFileStream(std::filesystem::path filePath, CreateFlags flags)
+      : Flags(flags),
         SourceFileName(std::move(filePath)),
-        FileStream(SourceFileName, GetFileMode(truncate)) {
+        FileStream(SourceFileName, PrepareFileOpenMode(flags, ErrorCode)) {
     Meta.FileName = SourceFileName.string();
   }
 
+  PhysicalFileStream(std::filesystem::path filePath)
+      : PhysicalFileStream(std::move(filePath), CreateFlagsMode::READ) {}
+
   PhysicalFileStream(PhysicalFileStream const& other)
-      : SourceFileName(other.SourceFileName),
-        FileStream(other.SourceFileName, GetFileMode(other.Truncate)) {
+      : Flags(other.Flags),
+        SourceFileName(other.SourceFileName),
+        FileStream(other.SourceFileName,
+                   PrepareFileOpenMode(Flags, ErrorCode)) {
     Meta.FileName = SourceFileName.string();
   }
-  bool Truncate;
+  std::error_code ErrorCode;
+  CreateFlags Flags;
   std::filesystem::path SourceFileName;
   std::fstream FileStream;
 };
