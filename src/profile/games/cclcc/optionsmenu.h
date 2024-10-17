043f7e6b93b10acb7c66d476d875ad198eff2937
PringlesGang <liamelderbroek123@gmail.com>
2024-10-17 20:44:04 +0200

    Added cclcc config sprites to cpp profile

--- src/profile/games/cclcc/optionsmenu.h
+++ src/profile/games/cclcc/optionsmenu.h
@@ -1,11 +1,42 @@
 #pragma once
 
 #include "../../../spritesheet.h"
+#include "../../../spriteanimation.h"
 
 namespace Impacto {
 namespace Profile {
 namespace CCLCC {
 namespace OptionsMenu {
+
+int constexpr PageHeaderSpriteCount = 4;
+int constexpr PagePanelSpriteCount = 8;
+int constexpr LabelSpriteCount = 16;
+int constexpr NametagSpriteCount = 13;
+int constexpr PortraitSpriteCount = 26;
+
+inline Sprite PointerSprite;
+
+inline Sprite HeaderSprite;
+inline Sprite PageHeaderSprites[PageHeaderSpriteCount];
+inline SpriteAnimationDef PoleAnimation;
+
+inline Sprite SliderTrackSprite;
+inline Sprite VoiceSliderTrackSprite;
+inline Sprite BinaryBoxSprite;
+
+inline Sprite SkipReadSprite;
+inline Sprite SkipAllSprite;
+inline Sprite OnSprite;
+inline Sprite OffSprite;
+inline Sprite YesSprite;
+inline Sprite NoSprite;
+
+inline Sprite GuideSprite;
+inline Sprite VoiceGuideSprite;
+
+inline Sprite LabelSprites[LabelSpriteCount];
+inline Sprite NametagSprites[NametagSpriteCount];
+inline Sprite PortraitSprites[PortraitSpriteCount];
 
 void Configure();
 
