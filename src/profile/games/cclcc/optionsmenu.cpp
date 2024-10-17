659479525f6a53c393fe4f8b16f731b72cae4a84
PringlesGang <liamelderbroek123@gmail.com>
2024-10-17 20:44:04 +0200

    Added cclcc config sprites to cpp profile

--- src/profile/games/cclcc/optionsmenu.cpp
+++ src/profile/games/cclcc/optionsmenu.cpp
@@ -13,6 +13,29 @@ namespace CCLCC {
 namespace OptionsMenu {
 
 void Configure() {
+  HeaderSprite = EnsureGetMemberSprite("HeaderSprite");
+  GetMemberSpriteArray(PageHeaderSprites, PageHeaderSpriteCount,
+                       "PageHeaderSprites");
+  PoleAnimation = EnsureGetMemberAnimation("PoleAnimation");
+
+  SliderTrackSprite = EnsureGetMemberSprite("SliderTrackSprite");
+  VoiceSliderTrackSprite = EnsureGetMemberSprite("VoiceSliderTrackSprite");
+  BinaryBoxSprite = EnsureGetMemberSprite("BinaryBoxSprite");
+
+  SkipReadSprite = EnsureGetMemberSprite("SkipReadSprite");
+  SkipAllSprite = EnsureGetMemberSprite("SkipAllSprite");
+  OnSprite = EnsureGetMemberSprite("OnSprite");
+  OffSprite = EnsureGetMemberSprite("OffSprite");
+  YesSprite = EnsureGetMemberSprite("YesSprite");
+  NoSprite = EnsureGetMemberSprite("NoSprite");
+
+  GuideSprite = EnsureGetMemberSprite("GuideSprite");
+  VoiceGuideSprite = EnsureGetMemberSprite("VoiceGuideSprite");
+
+  GetMemberSpriteArray(LabelSprites, LabelSpriteCount, "LabelSprites");
+  GetMemberSpriteArray(NametagSprites, NametagSpriteCount, "NametagSprites");
+  GetMemberSpriteArray(PortraitSprites, PortraitSpriteCount, "PortraitSprites");
+
   auto drawType = Game::DrawComponentType::_from_integral_unchecked(
       EnsureGetMemberInt("DrawType"));
 
