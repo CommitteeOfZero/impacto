127661f0dca0021652707399037b7fd8a8a31cd9
PringlesGang <liamelderbroek123@gmail.com>
2024-10-17 21:37:11 +0200

    Added positioning info to cclcc options profile

--- src/profile/games/cclcc/optionsmenu.cpp
+++ src/profile/games/cclcc/optionsmenu.cpp
@@ -13,14 +13,29 @@ namespace CCLCC {
 namespace OptionsMenu {
 
 void Configure() {
+  BackgroundPosition = EnsureGetMemberVec2("BackgroundPosition");
+
+  PointerSprite = EnsureGetMemberSprite("PointerSprite");
+  PointerOffset = EnsureGetMemberVec2("PointerOffset");
+
   HeaderSprite = EnsureGetMemberSprite("HeaderSprite");
+  HeaderPosition = EnsureGetMemberVec2("HeaderPosition");
   GetMemberSpriteArray(PageHeaderSprites, PageHeaderSpriteCount,
                        "PageHeaderSprites");
+  PageHeaderPosition = EnsureGetMemberVec2("PageHeaderPosition");
+
+  PagePanelSprite = EnsureGetMemberSprite("PagePanelSprite");
+  PagePanelPosition = EnsureGetMemberVec2("PagePanelPosition");
+  GetMemberSpriteArray(PagePanelSprites, PagePanelSpriteCount,
+                       "PagePanelSprites");
   PoleAnimation = EnsureGetMemberAnimation("PoleAnimation");
 
   SliderTrackSprite = EnsureGetMemberSprite("SliderTrackSprite");
+  SliderTrackOffset = EnsureGetMemberVec2("SliderTrackOffset");
   VoiceSliderTrackSprite = EnsureGetMemberSprite("VoiceSliderTrackSprite");
+  VoiceSliderOffset = EnsureGetMemberVec2("VoiceSliderOffset");
   BinaryBoxSprite = EnsureGetMemberSprite("BinaryBoxSprite");
+  BinaryBoxOffset = EnsureGetMemberVec2("BinaryBoxOffset");
 
   SkipReadSprite = EnsureGetMemberSprite("SkipReadSprite");
   SkipAllSprite = EnsureGetMemberSprite("SkipAllSprite");
@@ -31,10 +46,19 @@ void Configure() {
 
   GuideSprite = EnsureGetMemberSprite("GuideSprite");
   VoiceGuideSprite = EnsureGetMemberSprite("VoiceGuideSprite");
+  GuidePosition = EnsureGetMemberVec2("GuidePosition");
+
+  EntriesStartPosition = EnsureGetMemberVec2("EntriesStartPosition");
+  EntriesVerticalOffset = EnsureGetMemberInt("EntriesVerticalOffset");
+  SoundEntriesStartPosition = EnsureGetMemberVec2("SoundEntriesStartPosition");
+  SoundEntriesVerticalOffset = EnsureGetMemberInt("SoundEntriesVerticalOffset");
+  VoiceEntriesOffset = EnsureGetMemberVec2("VoiceEntriesOffset");
 
   GetMemberSpriteArray(LabelSprites, LabelSpriteCount, "LabelSprites");
   GetMemberSpriteArray(NametagSprites, NametagSpriteCount, "NametagSprites");
+  NametagOffset = EnsureGetMemberVec2("NametagOffset");
   GetMemberSpriteArray(PortraitSprites, PortraitSpriteCount, "PortraitSprites");
+  VoicePosition = EnsureGetMemberVec2("VoicePosition");
 
   auto drawType = Game::DrawComponentType::_from_integral_unchecked(
       EnsureGetMemberInt("DrawType"));
