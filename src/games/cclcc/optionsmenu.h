aa2482af8d54f2d77934c55ea9e095f4af3e48d5
PringlesGang <liamelderbroek123@gmail.com>
2024-10-20 22:44:57 +0200

    Implemented pages and binary buttons

--- src/games/cclcc/optionsmenu.h
+++ src/games/cclcc/optionsmenu.h
@@ -2,6 +2,7 @@
 
 #include "../../ui/menu.h"
 #include "../../spriteanimation.h"
+#include "../../ui/widgets/group.h"
 
 namespace Impacto {
 namespace UI {
@@ -17,8 +18,18 @@ class OptionsMenu : public Menu {
   void Render();
 
  private:
+  void GoToPage(int pageNumber);
+
   Animation FadeAnimation;
   SpriteAnimation PoleAnimation;
+
+  Widgets::Group* BasicPage;
+  Widgets::Group* TextPage;
+  Widgets::Group* SoundPage;
+  Widgets::Group* VoicePage;
+
+  int CurrentPage;
+  std::vector<UI::Widgets::Group*> Pages;
 };
 
 }  // namespace CCLCC
