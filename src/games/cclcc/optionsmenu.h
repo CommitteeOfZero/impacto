c5b4da3a736236f942c9be8d3d24c765cb313834
PringlesGang <liamelderbroek123@gmail.com>
2024-11-12 22:11:51 +0100

    Implemented base of config movement

--- src/games/cclcc/optionsmenu.h
+++ src/games/cclcc/optionsmenu.h
@@ -12,10 +12,11 @@ class OptionsMenu : public Menu {
  public:
   OptionsMenu();
 
-  void Show();
-  void Hide();
-  void Update(float dt);
-  void Render();
+  void Show() override;
+  void Hide() override;
+  void Update(float dt) override;
+  void UpdateInput() override;
+  void Render() override;
 
  private:
   void GoToPage(int pageNumber);
