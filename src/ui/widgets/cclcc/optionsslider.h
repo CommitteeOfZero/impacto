5501dcd9fee0f46382cd4128a196ae2a8275a088
PringlesGang <liamelderbroek123@gmail.com>
2024-11-12 22:54:04 +0100

    Implemented config slider movement

--- src/ui/widgets/cclcc/optionsslider.h
+++ src/ui/widgets/cclcc/optionsslider.h
@@ -11,15 +11,16 @@ namespace CCLCC {
 class OptionsSlider : public OptionsEntry {
  public:
   OptionsSlider(const Sprite& box, const Sprite& label, glm::vec2 pos,
-                glm::vec4 highlightTint);
+                glm::vec4 highlightTint, float sliderSpeed);
 
   void Render() override;
-  void UpdateInput() override;
+  void Update(float dt) override;
 
  private:
   const Sprite& BoxSprite;
 
   float Progress = 0.0f;
+  float SliderSpeed = 1.0f;
 };
 
 }  // namespace CCLCC
