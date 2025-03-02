93e6634499f73fb0f6c5d9209bc7ab03aa9954c4
Dextinfire <>
2025-03-01 16:23:19 -0800

    Add in precompiled headers

---
+++ src/pch.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#define NOMINMAX
+
+#include <algorithm>
+#include <chrono>
+#include <unordered_map>
+#include <map>
+#include <memory>
+#include <optional>
+#include <string>
+#include <utility>
+#include <vector>
+#include <random>
+
+#include <cstdio>
+#include <cassert>
+#include <ctime>
+#include <cstring>
+#include <cstdlib>
+#include <cstdint>
+
+#include <SDL.h>
+#include <flat_hash_map.hpp>
+#include <span/span.hpp>
+#include <glm/glm.hpp>
+#include <enum.h>
