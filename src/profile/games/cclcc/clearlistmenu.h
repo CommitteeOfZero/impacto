60291b0887ca45e8a8d330f67aba9538d8deea4a
Enorovan <enorovan@gmail.com>
2025-04-05 22:23:04 +0200

    cclcc/clearlist: implement clearlist

--- src/profile/games/cclcc/clearlistmenu.h
+++ src/profile/games/cclcc/clearlistmenu.h
@@ -7,14 +7,20 @@ namespace Profile {
 namespace CCLCC {
 namespace ClearListMenu {
 
+int constexpr Endings = 10;
+
 inline Sprite ClearListBookLayerSprite;
+inline Sprite ClearListCompleteBookLayerSprite;
 inline Sprite ClearListGuideSprite;
-inline Sprite LibraryMaskSprite;
+inline Sprite ClearListMaskSprite;
+
+inline Sprite EndingSprites[Endings];
+inline float EndingSpriteOffsetY;
+inline float MenuOffsetY;
 
 inline float FadeInDuration;
 inline float FadeOutDuration;
-inline int ClearListGuideX;
-inline int ClearListGuideY;
+inline glm::vec2 ClearListGuidePosition;
 
 void Configure();
 
