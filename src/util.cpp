0e04d1543e2aa14046696721f2601ceedee4764e
PringlesGang <liamelderbroek123@gmail.com>
2025-04-27 21:12:13 +0200

    Added AxisAnglesToQuaternion function

--- src/util.cpp
+++ src/util.cpp
@@ -28,7 +28,7 @@ glm::mat2 Rotate2D(float angle) {
 glm::mat4 Transformation2D(const glm::vec2 translation, const glm::vec2 origin,
                            const float rotation, const glm::vec2 scaling) {
   return Transformation3D({translation, 0.0f}, {origin, 0.0f},
-                          glm::quat(rotation, {0.0f, 0.0f, 1.0f}),
+                          AxisAngleToQuaternion({0.0f, 0.0f, 1.0f}, rotation),
                           {scaling, 1.0f});
 }
 
@@ -37,7 +37,7 @@ glm::mat4 Transformation3D(const glm::vec3 translation, const glm::vec3 origin,
   glm::mat4 matrix(1.0f);
   matrix = glm::translate(glm::mat4(1.0f), -origin) * matrix;
   matrix = glm::scale(glm::mat4(1.0f), scaling) * matrix;
-  matrix = glm::mat4_cast(rotation) * matrix;
+  matrix = glm::mat4_cast(normalize(rotation)) * matrix;
   matrix = glm::translate(glm::mat4(1.0f), origin + translation) * matrix;
   return matrix;
 }
