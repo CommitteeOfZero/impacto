a919a06a1f0a64d5622f8e61cd69eba2b490aa91
PringlesGang <liamelderbroek123@gmail.com>
2025-05-03 23:34:39 +0200

    Disabled Vulkan face culling

--- src/renderer/vulkan/pipeline.cpp
+++ src/renderer/vulkan/pipeline.cpp
@@ -23,8 +23,7 @@ Pipeline::Pipeline(VkDevice device, VkRenderPass renderPass) {
   rasterizer.rasterizerDiscardEnable = VK_FALSE;
   rasterizer.polygonMode = VK_POLYGON_MODE_FILL;
   rasterizer.lineWidth = 1.0f;
-  rasterizer.cullMode = VK_CULL_MODE_BACK_BIT;
-  rasterizer.frontFace = VK_FRONT_FACE_COUNTER_CLOCKWISE;
+  rasterizer.cullMode = VK_CULL_MODE_NONE;
   rasterizer.depthBiasEnable = VK_FALSE;
   RasterizerInfo = rasterizer;
 
