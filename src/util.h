231b2bf95b31e26e4851f527f3f5a827edade7dd
PringlesGang <liamelderbroek123@gmail.com>
2025-06-13 16:33:15 +0200

    Changed is_poc requirement to modern is_trivially_copyable

--- src/util.h
+++ src/util.h
@@ -439,7 +439,8 @@ inline void MakeLowerCase(std::string& str) {
 
 template <typename T>
 T UnalignedRead(void* ptr) {
-  static_assert(std::is_pod<T>::value, "!std::is_pod<T>");
+  static_assert(std::is_trivially_copyable<T>::value,
+                "!std::is_trivially_copyable<T>");
   T value;
   memcpy(&value, ptr, sizeof value);
   return value;
@@ -447,7 +448,8 @@ T UnalignedRead(void* ptr) {
 
 template <typename T>
 void UnalignedWrite(void* ptr, T value) {
-  static_assert(std::is_pod<T>::value, "!std::is_pod<T>");
+  static_assert(std::is_trivially_copyable<T>::value,
+                "!std::is_trivially_copyable<T>");
   memcpy(ptr, &value, sizeof value);
 }
 
