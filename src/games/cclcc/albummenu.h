46f9b0331ee9eb9376fed4b7b6f043448243429d
Dextinfire <>
2025-07-11 02:37:33 -0700

    Basic Album Menu

--- src/games/cclcc/albummenu.h
+++ src/games/cclcc/albummenu.h
@@ -5,9 +5,45 @@
 namespace Impacto {
 namespace UI {
 namespace CCLCC {
+
+struct AlbumThumbnail : public Widgets::Button {
+  enum class DisplayState : uint8_t {
+    Hidden,
+    Hiding,
+    Showing,
+    Shown,
+  };
+
+  AlbumThumbnail(int id, uint8_t const& activePage);
+  void UpdateInput() override;
+  void Render() override;
+  void Show() override {
+    if (State == DisplayState::Hidden && ActivePage == Page) {
+      State = DisplayState::Shown;
+    }
+  }
+
+  void Hide() override {
+    if (State == DisplayState::Shown) {
+      State = DisplayState::Hidden;
+    }
+  }
+
+  DisplayState State = DisplayState::Hidden;
+  uint8_t Page;
+  uint8_t IndexInPage;
+  uint8_t const& ActivePage;
+};
+
 class AlbumMenu : public LibrarySubmenu {
  public:
   AlbumMenu() = default;
+  void Init() override;
+  void Render() override;
+  void Update(float dt) override;
+
+  std::vector<std::vector<AlbumThumbnail*>> ThumbnailPages;
+  uint8_t ActivePage = 0;
 };
 }  // namespace CCLCC
 }  // namespace UI
