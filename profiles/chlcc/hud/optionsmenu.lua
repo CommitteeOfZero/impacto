a0370687916722b5bd1550f227899b3147a3e25f
PringlesGang <liamelderbroek123@gmail.com>
2025-09-02 22:12:54 +0200

    Set chlcc options menu sprites in profile

--- profiles/chlcc/hud/optionsmenu.lua
+++ profiles/chlcc/hud/optionsmenu.lua
@@ -1,15 +1,21 @@
 root.OptionsMenu = {
     DrawType = DrawComponentType.SystemMenu,
     Type = OptionsMenuType.CHLCC,
+
     FadeInDuration = 64 / 60,
     FadeOutDuration = 64 / 60,
+
     BackgroundColor = 0xa988e5,
+
     CircleSprite = "CircleConfig",
     CircleStartPosition = { X = 20, Y = 20 },
     CircleOffset = 200,
+
     ErinSprite = "Erin",
     ErinPosition = { X = 301, Y = 1 },
+
     BackgroundFilter = "BackgroundFilter",
+
     InitialRedBarPosition = { X = 0, Y = 538 },
     RightRedBarPosition = { X = 1059, Y = 538 },
     RedBarDivision = 1826,
@@ -17,29 +23,49 @@ root.OptionsMenu = {
     RedBarSprite = "RedBar",
     RedBarLabelPosition = { X = 1067, Y = 573 },
     RedBarLabel = "RedBarLabel",
+
     MenuTitleTextRightPos = { X = 550, Y = 525 },
     MenuTitleTextAngle = 6.02,
     TitleFadeInDuration = 40 / 60,
     TitleFadeOutDuration = 28 / 60,
     MenuTitleText = "MenuTitleTextConfig",
+
     ButtonPromptSprite = "ButtonPromptConfig",
     ButtonPromptPosition = { X = 685, Y = 651 },
-    SelectWord = {},
-    SelectWordPos = {
-        { X = 94, Y = 51 },
-        { X = 109, Y = 51 },
-        { X = 122, Y = 51 },
-        { X = 134, Y = 51 },
-        { X = 147, Y = 51 },
-        { X = 161, Y = 51 },
-        { X = 180, Y = 51 },
-        { X = 199, Y = 51 },
-        { X = 213, Y = 51 },
-        { X = 228, Y = 51 }
-    },
 
     MinButtonHoldTime = 0.5,
     ButtonHoldFireInterval = 0.07,
+
+    SelectedSprite = "SelectedSprite",
+    SelectedLabelSprite = "SelectedLabelSprite",
+    SelectedDotSprite = "SelectedDotSprite",
+    VoiceMutedSprite = "VoiceMutedSprite",
+
+    BasicSettingsSprite = "BasicSettingsSprite",
+    TextSettingsSprite = "TextSettingsSprite",
+    SoundSettingsSprite = "SoundSettingsSprite",
+    VoiceSettingsSprite = "VoiceSettingsSprite",
+
+    SliderBarBaseSprite = "SliderBarBaseSprite",
+    SliderBarFillSprite = "SliderBarFillSprite",
+
+    SettingInstantSprite = "SettingInstantSprite",
+    SettingFastSprite = "SettingFastSprite",
+    SettingNormalSprite = "SettingNormalSprite",
+    SettingSlowSprite = "SettingSlowSprite",
+    SettingShortSprite = "SettingShortSprite",
+    SettingLongSprite = "SettingLongSprite",
+    SettingDoSprite = "SettingDoSprite",
+    SettingDontSprite = "SettingDontSprite",
+    SettingYesSprite = "SettingYesSprite",
+    SettingNoSprite = "SettingNoSprite",
+    SettingReadSprite = "SettingReadSprite",
+    SettingAllSprite = "SettingAllSprite",
+    SettingOnTriggerSprite = "SettingOnTriggerSprite",
+    SettingOnSceneSprite = "SettingOnSceneSprite",
+    SettingOnTriggerAndSceneSprite = "SettingOnTriggerAndSceneSprite",
+    SettingTypeASprite = "SettingTypeASprite",
+    SettingTypeBSprite = "SettingTypeBSprite",
 };
 
 root.Sprites["CircleConfig"] = {
@@ -75,4 +101,141 @@ root.Sprites["MenuTitleTextConfig"] = {
 root.Sprites["ButtonPromptConfig"] = {
     Sheet = "Options",
     Bounds = { X = 481, Y = 824, Width = 676, Height = 28 }
+}
+
+root.Sprites["SelectedSprite"] = {
+    Sheet = "Options",
+    -- Yes, the single-pixel-wide overhang is in the binary
+    Bounds = { X = 719, Y = 766, Width = 232, Height = 40 }
+}
+
+root.Sprites["SelectedLabelSprite"] = {
+    Sheet = "Options",
+    Bounds = { X = 480, Y = 724, Width = 630, Height = 40 }
+}
+
+root.Sprites["SelectedDotSprite"] = {
+    Sheet = "Options",
+    Bounds = { X = 42, Y = 602, Width = 18, Height = 18 }
+}
+
+root.Sprites["VoiceMutedSprite"] = {
+    Sheet = "Options",
+    Bounds = { X = 1, Y = 602, Width = 39, Height = 39 }
+}
+
+root.Sprites["BasicSettingsSprite"] = {
+    Sheet = "Options",
+    Bounds = { X = 453, Y = 1, Width = 574, Height = 314 }
+}
+
+root.Sprites["TextSettingsSprite"] = {
+    Sheet = "Options",
+    Bounds = { X = 453, Y = 315, Width = 504, Height = 284 }
+}
+
+root.Sprites["SoundSettingsSprite"] = {
+    Sheet = "Options",
+    Bounds = { X = 109, Y = 602, Width = 369, Height = 369 }
+}
+
+root.Sprites["VoiceSettingsSprite"] = {
+    Sheet = "Options",
+    Bounds = { X = 1, Y = 1, Width = 450, Height = 599 }
+}
+
+root.Sprites["SliderBarBaseSprite"] = {
+    Sheet = "Options",
+    Bounds = { X = 480, Y = 766, Width = 237, Height = 27 }
+}
+
+root.Sprites["SliderBarFillSprite"] = {
+    Sheet = "Options",
+    Bounds = { X = 486, Y = 795, Width = 225, Height = 27 }
+}
+
+-- Setting sprites
+root.Sprites["SettingInstantSprite"] = {
+    Sheet = "Options",
+    Bounds = { X = 1121, Y = 1, Width = 56, Height = 28 }
+}
+
+root.Sprites["SettingFastSprite"] = {
+    Sheet = "Options",
+    Bounds = { X = 1121, Y = 31, Width = 56, Height = 28 }
+}
+
+root.Sprites["SettingNormalSprite"] = {
+    Sheet = "Options",
+    Bounds = { X = 1121, Y = 61, Width = 56, Height = 28 }
+}
+
+root.Sprites["SettingSlowSprite"] = {
+    Sheet = "Options",
+    Bounds = { X = 1121, Y = 91, Width = 56, Height = 28 }
+}
+
+root.Sprites["SettingShortSprite"] = {
+    Sheet = "Options",
+    Bounds = { X = 1121, Y = 121, Width = 56, Height = 28 }
+}
+
+root.Sprites["SettingLongSprite"] = {
+    Sheet = "Options",
+    Bounds = { X = 1121, Y = 151, Width = 56, Height = 28 }
+}
+
+root.Sprites["SettingDoSprite"] = {
+    Sheet = "Options",
+    Bounds = { X = 1121, Y = 181, Width = 56, Height = 28 }
+}
+
+root.Sprites["SettingDontSprite"] = {
+    Sheet = "Options",
+    Bounds = { X = 1099, Y = 211, Width = 78, Height = 28 }
+}
+
+root.Sprites["SettingYesSprite"] = {
+    Sheet = "Options",
+    Bounds = { X = 1121, Y = 241, Width = 56, Height = 28 }
+}
+
+root.Sprites["SettingNoSprite"] = {
+    Sheet = "Options",
+    Bounds = { X = 1099, Y = 271, Width = 78, Height = 28 }
+}
+
+root.Sprites["SettingReadSprite"] = {
+    Sheet = "Options",
+    Bounds = { X = 1069, Y = 301, Width = 108, Height = 28 }
+}
+
+root.Sprites["SettingAllSprite"] = {
+    Sheet = "Options",
+    Bounds = { X = 1121, Y = 331, Width = 56, Height = 28 }
+}
+
+root.Sprites["SettingOnTriggerSprite"] = {
+    Sheet = "Options",
+    Bounds = { X = 1047, Y = 361, Width = 130, Height = 28 }
+}
+
+root.Sprites["SettingOnSceneSprite"] = {
+    Sheet = "Options",
+    Bounds = { X = 1068, Y = 391, Width = 108, Height = 28 }
+}
+
+root.Sprites["SettingOnTriggerAndSceneSprite"] = {
+    Sheet = "Options",
+    Bounds = { X = 959, Y = 421, Width = 218, Height = 28 }
+}
+
+root.Sprites["SettingTypeASprite"] = {
+    Sheet = "Options",
+    Bounds = { X = 1068, Y = 451, Width = 108, Height = 28 }
+}
+
+root.Sprites["SettingTypeBSprite"] = {
+    Sheet = "Options",
+    Bounds = { X = 1068, Y = 481, Width = 108, Height = 28 }
 }
--- src/profile/games/chlcc/optionsmenu.h
+++ src/profile/games/chlcc/optionsmenu.h
@@ -8,28 +8,65 @@ namespace CHLCC {
 namespace OptionsMenu {
 
 inline uint32_t BackgroundColor;
+
 inline Sprite CircleSprite;
 inline glm::vec2 CircleStartPosition;
 inline float CircleOffset;
-inline glm::vec2 ErinPosition;
+
 inline Sprite ErinSprite;
+inline glm::vec2 ErinPosition;
+
 inline Sprite BackgroundFilter;
+
 inline glm::vec2 InitialRedBarPosition;
-inline Sprite InitialRedBarSprite;
 inline glm::vec2 RightRedBarPosition;
-inline glm::vec2 RedBarPosition;
 inline float RedBarDivision;
 inline float RedBarBaseX;
 inline Sprite RedBarSprite;
+inline Sprite InitialRedBarSprite;
+inline glm::vec2 RedBarPosition;
 inline glm::vec2 RedBarLabelPosition;
 inline Sprite RedBarLabel;
+
 inline glm::vec2 MenuTitleTextRightPosition;
 inline float MenuTitleTextAngle;
 inline float TitleFadeInDuration;
 inline float TitleFadeOutDuration;
 inline Sprite MenuTitleText;
-inline glm::vec2 ButtonPromptPosition;
+
 inline Sprite ButtonPromptSprite;
+inline glm::vec2 ButtonPromptPosition;
+
+inline Sprite SelectedSprite;
+inline Sprite SelectedLabelSprite;
+inline Sprite SelectedDotSprite;
+inline Sprite VoiceMutedSprite;
+
+inline Sprite BasicSettingsSprite;
+inline Sprite TextSettingsSprite;
+inline Sprite SoundSettingsSprite;
+inline Sprite VoiceSettingsSprite;
+
+inline Sprite SliderBarBaseSprite;
+inline Sprite SliderBarFillSprite;
+
+inline Sprite SettingInstantSprite;
+inline Sprite SettingFastSprite;
+inline Sprite SettingNormalSprite;
+inline Sprite SettingSlowSprite;
+inline Sprite SettingShortSprite;
+inline Sprite SettingLongSprite;
+inline Sprite SettingDoSprite;
+inline Sprite SettingDontSprite;
+inline Sprite SettingYesSprite;
+inline Sprite SettingNoSprite;
+inline Sprite SettingReadSprite;
+inline Sprite SettingAllSprite;
+inline Sprite SettingOnTriggerSprite;
+inline Sprite SettingOnSceneSprite;
+inline Sprite SettingOnTriggerAndSceneSprite;
+inline Sprite SettingTypeASprite;
+inline Sprite SettingTypeBSprite;
 
 void Configure();
 
