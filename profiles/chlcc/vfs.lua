d24a9d27c60fe7599cf5548156a3a4e3984ba126
Enorovan <enorovan@gmail.com>
2025-10-24 10:37:05 +0200

    chlcc/vfs-case-sensitivity

--- profiles/chlcc/vfs.lua
+++ profiles/chlcc/vfs.lua
@@ -1,13 +1,13 @@
 root.Vfs = {
     Mounts = {
-        ["script"] = {"games/chlcc/gamedata/script.cpk"},
-        ["system"] = {"games/chlcc/gamedata/system.cpk"},
-        ["bgm"] = {"games/chlcc/gamedata/bgm.cpk"},
-        ["se"] = {"games/chlcc/gamedata/se.cpk"},
-        ["voice"] = {"games/chlcc/gamedata/voice.cpk"},
-        ["bg"] = {"games/chlcc/gamedata/bg.cpk"},
-        ["chara"] = {"games/chlcc/gamedata/chara.cpk"},
-        ["mask"] = {"games/chlcc/gamedata/mask.cpk"},
-        ["movie"] = {"games/chlcc/gamedata/movie.cpk"}
+        ["bg"] = {"games/chlcc/gamedata/BG.CPK"},
+        ["bgm"] = {"games/chlcc/gamedata/BGM.CPK"},
+        ["chara"] = {"games/chlcc/gamedata/CHARA.CPK"},
+        ["mask"] = {"games/chlcc/gamedata/MASK.CPK"},
+        ["movie"] = {"games/chlcc/gamedata/MOVIE.CPK"},
+        ["script"] = {"games/chlcc/gamedata/SCRIPT.CPK"},
+        ["se"] = {"games/chlcc/gamedata/SE.CPK"},
+        ["system"] = {"games/chlcc/gamedata/SYSTEM.CPK"},
+        ["voice"] = {"games/chlcc/gamedata/VOICE.CPK"}
     }
 };
