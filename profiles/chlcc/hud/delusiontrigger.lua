57caeec91124845365778f124d65e8fc72effee8
Dextinfire <>
2025-11-14 03:20:29 -0800

    Working JP delusion text

--- profiles/chlcc/hud/delusiontrigger.lua
+++ profiles/chlcc/hud/delusiontrigger.lua
@@ -2,6 +2,8 @@ root.DelusionTrigger = {
   BackgroundSprite = "BackgroundSprite",
   BackgroundSpriteMask = "BackgroundSpriteMask",
   ScreenMask = "ScreenMask",
+
+  DelusionTextGlyphs = {},
 };
 
 root.Sprites["BackgroundSprite"] = {
@@ -16,3 +18,27 @@ root.Sprites["BackgroundSpriteMask"] = {
   Sheet = "DelusionMask",
   Bounds = { X = 0, Y = 0, Width = 1024, Height = 1024 }
 }
+
+local textCharacterCount = {
+  13,	13,	12,	17, 16,	17,	13,	13,
+  14,	19,	16,	16, 16,	12,	10,	15,
+  12,	20,	15,	14, 15,
+}
+
+for i = 0, 21 - 1 do
+  local currentLine = {}
+  for j = 0, textCharacterCount[i + 1] - 1 do
+      local name = "DelusionTextGlyph" .. i .. "_" .. j
+      root.Sprites[name] = {
+          Sheet = "DelusionText",
+          Bounds = {
+              X = j * 64,
+              Y = i * 64,
+              Width = 64,
+              Height = 64
+          },
+      };
+      currentLine[j + 1] = name;
+  end
+  root.DelusionTrigger.DelusionTextGlyphs[i + 1] = currentLine
+end
--- src/games/chlcc/delusiontrigger.h
+++ src/games/chlcc/delusiontrigger.h
@@ -6,6 +6,32 @@
 namespace Impacto {
 namespace UI {
 namespace CHLCC {
+class DelusionTrigger;
+class DelusionTextSystem {
+ public:
+  static int constexpr LineWidth = 60;
+  std::array<std::array<const Sprite*, 60>, 3> GlyphLines;
+  std::array<size_t, 300> LineOffsets{};
+
+  size_t TextIndex = 0;
+  int TextLineXOffset = 0;
+  const DelusionTrigger* Ctx;
+
+  DelusionTextSystem(DelusionTrigger const& ctx);
+
+  void Init();
+  void Update();
+  void Render();
+  void Clear();
+
+ private:
+  std::optional<size_t> LineBase() const;
+  void InitLineOffsets();
+  void InitLines();
+  void ScrollLine(int lineIndex);
+  void ScrollAll();
+};
+
 class DelusionTrigger {
  public:
   enum DelusionState { DS_Neutral, DS_Positive, DS_Negative };
@@ -31,6 +57,8 @@ class DelusionTrigger {
   void UpdateHiding();
   void PlayClickSound();
 
+  int& DelusionState;
+
   int MaskScaleFactor;
   int SpinAngle;
   int SpinRate;
@@ -46,7 +74,12 @@ class DelusionTrigger {
   int UnderlayerXOffset, UnderlayerXRate;
   int ShakeState;
   int MaskOffsetX;
-  int& DelusionState;
+  std::optional<int> DelusionSelectedLine;
+  int DelusionTextAlpha;
+
+  DelusionTextSystem TextSystem;
+
+  friend DelusionTextSystem;
 };
 
 }  // namespace CHLCC
