418c1bb6d146e88cd0784b06814fad39bcec0b2c
newo-2001 <o.elderbroek@gmail.com>
2026-01-05 23:20:50 +0100

    Implement blur effect

--- src/shaders/opengl/GaussianBlur_frag.glsl
+++ src/shaders/opengl/GaussianBlur_frag.glsl
@@ -17,30 +17,38 @@ float WEIGHTS[WEIGHTS_COUNT] = float[](
     0.004433
 );
 
+/*
+    To prevent banding on higher resolutions, we sample each pixel over the requested distance,
+    instead of merely at four equidistant points. We then lerp the weights respecting the blur
+    distance the original game expects on a 480x255 texture, but on a texture of TextureDimensions
+    instead.
+*/
+float interpolateWeight(float progress) {
+    int lowerWeightIdx = min(int(floor(progress)), WEIGHTS_COUNT - 2);
+    int upperWeightIdx = lowerWeightIdx + 1;
+
+    float weightProgress = clamp(progress - lowerWeightIdx, 0.0, 1.0);
+    return mix(WEIGHTS[lowerWeightIdx], WEIGHTS[upperWeightIdx], weightProgress);
+}
+
 void main() {
     // Divide by TextureDimensions to normalize sprite dimensions to [0.0, 1.0]
     vec2 normalizedOffset = (IsHorizontal ? vec2(1.0, 0.0) : vec2(0.0, 1.0)) / TextureDimensions;
 
     // Vita's blur effect operates on a 480 x 255 texture
-    float pixelSize = IsHorizontal ? TextureDimensions.x / 480.0 : TextureDimensions.y / 255.0;
-    int weightDistance = max(int(round(sqrt(pixelSize))), 1);
-    int distance = weightDistance * WEIGHTS_COUNT;
-
-    /*
-        To prevent banding on higher resolutions, we sample a discrete uniform distribution
-        across the size that a single 480x255-texture pixel would cover on TextureDimensions,
-        effectively sampling the same constant weights at a different resolution
-    */
-    float uniformNormalizer = 1.0 / weightDistance;
+    float weightDistance = IsHorizontal ? TextureDimensions.x / 480.0 : TextureDimensions.y / 255.0;
+    int maxDistance = int(ceil(weightDistance * WEIGHTS_COUNT));
 
     color = vec4(0.0);
-    for (int i = 0; i < distance; i++) {
-        int weightIdx = i / weightDistance;
-        float weight = WEIGHTS[weightIdx] * uniformNormalizer;
+    float totalWeight = 0.0;
+    for (int i = 0; i < maxDistance; i++) {
+        float weight = interpolateWeight(float(i) / weightDistance);
+        totalWeight += weight * 2.0;
 
         color += weight * texture2D(ColorMap, uv + normalizedOffset * i);
         color += weight * texture2D(ColorMap, uv - normalizedOffset * i);
     }
+    color /= totalWeight;
 
     color.a = 1.0;
     color *= tint;
