d2e7777f764ab2f5d19201b0b848e718b0d0d847
PringlesGang <liamelderbroek123@gmail.com>
2026-06-03 00:00:09 +0200

    Allow for glyphs to be mapped to multiple indices in the charset (in particular for italics support)

--- profiles/common/charset.lua
+++ profiles/common/charset.lua
@@ -11,7 +11,6 @@ function AddCharsetFlag(characterString, flag)
         for targetBytePos, targetCodePoint in utf8.codes(root.CharsetInternal.CharsetStr) do
             if sourceCodePoint == targetCodePoint then
                 root.Charset.Flags[charsetIndex] = root.Charset.Flags[charsetIndex] | flag;
-                break;
             end
 
             charsetIndex = charsetIndex + 1;
