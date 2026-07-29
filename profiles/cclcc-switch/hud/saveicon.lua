1a03b10c8b2f7fda355600cc09eaa9b4edbfd6e8
Gunoshozo <gunoshozo@gmail.com>
2026-07-29 17:27:39 +0400

    Sprite size fixes - Sysmenu asset - Save & Wait Icon

---
+++ profiles/cclcc-switch/hud/saveicon.lua
@@ -0,0 +1,31 @@
+root.SaveIcon = {
+    ForegroundAnimation = "SaveIcon",
+    DefaultPosition = { X = 104, Y = 736 },
+    BackgroundSprite = "SaveIconBg",
+    BackgroundOffset = { X = 0, Y = 0 },
+    BackgroundMaxAlpha = 0.0,
+    FadeInDuration = 16/60,
+    FadeOutDuration = 12/60,
+    FullyVisibleSpriteIndex = 1
+};
+
+MakeAnimation({
+    Name = "SaveIcon",
+    Sheet = "Data",
+    FirstFrameX = 127,
+    FirstFrameY = 417,
+    FrameWidth = 179,
+    ColWidth = 179,
+    FrameHeight = 62,
+    RowHeight = 62,
+    Frames = 2,
+    Duration = 2*18/60, -- 18 frames per sprite
+    Rows = 2,
+    Columns = 1,
+    PrimaryDirection = AnimationDirections.Down
+});
+
+root.Sprites["SaveIconBg"] = {
+    Sheet = "Data",
+    Bounds = { X = 127, Y = 479, Width = 179, Height = 62 }
+};
--- profiles/cclcc-switch/sprites.lua
+++ profiles/cclcc-switch/sprites.lua
@@ -30,7 +30,7 @@ root.SpriteSheets = {
       DesignHeight = 3272
   },
   ["Data"] = {
-      Path = { Mount = "system", Id = 5 },
+      Path = { Mount = "system", Id = 26 },
       DesignWidth = 3072,
       DesignHeight = 1536
   },
@@ -67,7 +67,7 @@ root.SpriteSheets = {
   ["MenuBG"] = {
       Path = { Mount = "system", Id = 31 },
       DesignWidth = 3000,
-      DesignHeight = 1500
+      DesignHeight = 3000
   },
   ["MenuChip"] = {
       Path = { Mount = "system", Id = 11 },
