719bbecae9645e01724247cd33735a5b9bbee022
Dextinfire <>
2026-08-15 03:56:30 -0700

    Working launcher

--- src/overlay.cpp
+++ src/overlay.cpp
@@ -6,6 +6,8 @@
 #include "log.h"
 #include "inputsystem.h"
 #include "profile/game.h"
+#include "profile/gamedefinitions.h"
+#include "userconfig.h"
 
 namespace Impacto::Overlay {
 
@@ -31,9 +33,64 @@ void ShowOverlay() {
   ImGui::SetNextWindowPos(viewport->WorkPos);
   ImGui::SetNextWindowSize(viewport->WorkSize);
   ImGui::SetNextWindowViewport(viewport->ID);
+  static const char* selectedGame = nullptr;
   if (ImGui::Begin("Overlay##DockArea", &OverlayShown, windowFlags)) {
+    if (!Profile::Game::HasInit) {  // Game selection
+      ImGui::BeginGroup();
+      bool isSelected = false;
+      bool isOpening = ImGui::IsWindowAppearing();
+
+      {
+        float windowWidth = ImGui::GetContentRegionAvail().x;
+        constexpr static float comboWidth = 200.0f;
+
+        ImGui::SetCursorPosX((windowWidth - comboWidth) * 0.5f);
+        ImGui::SetNextItemWidth(comboWidth);
+      }
+
+      if (ImGui::BeginCombo("Choose Game", selectedGame)) {
+        for (auto&& [gameKey, gameDef] : Profile::GameDefinitions) {
+          if (gameDef.Hidden) continue;
+          if (isOpening) {
+            selectedGame = gameKey.c_str();
+            isOpening = false;
+          }
+          isSelected = selectedGame == gameKey.c_str();
+          if (ImGui::Selectable(gameKey.c_str(), isSelected))
+            selectedGame = gameKey.c_str();
+          if (isSelected) ImGui::SetItemDefaultFocus();
+        }
+        ImGui::EndCombo();
+      }
+
+      if (selectedGame) {
+        float windowWidth = ImGui::GetContentRegionAvail().x;
+        constexpr static float btnWidth = 200.0f;
+
+        ImGui::SetCursorPosX((windowWidth - btnWidth) * 0.5f);
+        ImGui::SetNextItemWidth(btnWidth);
+
+        if (ImGui::Button("Start Game", ImVec2(80, 20))) {
+          UserConfig::SetActiveGame(selectedGame);
+          OverlayShown = false;
+        }
+      }
+      ImGui::EndGroup();
+    } else {
+      selectedGame = UserConfig::GetActiveGame().c_str();
+    }
+
     if (ImGui::BeginTabBar("MainTabs")) {
+      if (ImGui::BeginTabItem("About")) {
+        ImGui::Text("%.3f ms/frame (%.1f FPS)",
+                    1000.0f / ImGui::GetIO().Framerate,
+                    ImGui::GetIO().Framerate);
+        ImGui::EndTabItem();
+      }
       if (ImGui::BeginTabItem("Settings")) {
+        if (selectedGame == nullptr) {
+          ImGui::Text("Please select a game first.");
+        }
         ImGui::EndTabItem();
       }
 
@@ -47,15 +104,8 @@ void ShowOverlay() {
 
       ImGui::EndTabBar();
     }
-
-    ImGui::Text("%.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate,
-                ImGui::GetIO().Framerate);
   }
   ImGui::End();
-
-  if (!OverlayShown) {
-    // Hide other tabs.
-  }
 }
 
 void Show() {
