d982f724b3041ae07605bcad5f017a231da01557
Enorovan <enorovan@gmail.com>
2026-08-21 00:05:12 +0200

    achievement-system: common AchievementSystem - Profile and Patch based, no data in impacto. - C;HLCC TrophyMenu handling - Saving AchievementData alongside saves - Sunset AchievementSystemPS3

--- src/profile/profile.cpp
+++ src/profile/profile.cpp
@@ -22,6 +22,7 @@
 #include "../ui/ui.h"
 #include "../data/savesystem.h"
 #include "../data/achievementsystem.h"
+#include "../data/achievementsystemcommon.h"
 #include "../data/tipssystem.h"
 #include "../hud/datedisplay.h"
 #include "../hud/waiticondisplay.h"
@@ -160,6 +161,7 @@ static void DefineEnums() {
   DefineEnum<Impacto::Game::DrawComponentType>(LuaState);
   DefineEnum<SaveSystem::SaveDataType>(LuaState);
   DefineEnum<AchievementSystem::AchievementDataType>(LuaState);
+  DefineEnum<AchievementSystem::AchievementRarity>(LuaState);
   DefineEnum<TipsSystem::TipsSystemType>(LuaState);
   DefineEnum<UI::CommonMenuType>(LuaState);
   DefineEnum<UI::SystemMenuType>(LuaState);
