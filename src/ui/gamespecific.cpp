abe287ae33d19e227ce0465f852901c8ae453e45
Dextinfire <>
2026-09-06 23:52:29 -0700

    Map backlog swipe down to keyboard direct press.
    Handle @chan too

--- src/ui/gamespecific.cpp
+++ src/ui/gamespecific.cpp
@@ -264,7 +264,17 @@ static void UpdateCCAtChanScrollbar() {
     const float offset = bgYRange.first - (mouseYRange.first * slope);
     const int conversion = (int)(Input::CurMousePos.y * slope + offset);
 
-    if (Input::CurrentInputDevice == Input::Device::Mouse) {
+    // Don't open backlog when scrolling
+    if (Input::CurrentInputDevice == Input::Device::Mouse ||
+        Input::CurrentInputDevice == Input::Device::Touch) {
+      if (Input::MouseWheelDeltaY < 0 || Input::TouchFlickDown) {
+        auto kbBtnItr = Vm::Interface::KBcustom.find(52);
+        if (kbBtnItr != Vm::Interface::KBcustom.end()) {
+          for (auto scancode : kbBtnItr->second)
+            Input::KeyboardButtonWentDown[scancode] = false;
+        }
+      }
+
       ScrWork[SW_BG1POSY] -= static_cast<int>(Input::MouseWheelDeltaY * 16.0f);
       mouseHover = thumbBounds.ContainsPoint(Input::CurMousePos);
       if (mouseHover) {
--- src/vm/interface/input.cpp
+++ src/vm/interface/input.cpp
@@ -232,6 +232,15 @@ void UpdatePADInput() {
     UpdateFromPADCode(PADcode, PADInputType::IsDown);
   }
 
+  // Sync scroll down & touch flick down with backlog keyboard key.
+  if (Input::MouseWheelDeltaY < 0 || Input::TouchFlickDown) {
+    auto kbBtnItr = KBcustom.find(52);
+    if (kbBtnItr != KBcustom.end()) {
+      for (auto scancode : kbBtnItr->second)
+        Input::KeyboardButtonWentDown[scancode] = true;
+    }
+  }
+
   for (auto const& [PADcode, mouseButton] : PADToMouse) {
     if (mouseButton == SDL_BUTTON_LEFT) {
       if (Input::TouchTapCount == 1) PADinputMouseWentDown |= PADcode;
